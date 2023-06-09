6b789791bc5c46605ca8757de1f879078a368b37
Shota Sabashvili <s.sabashvili@ggtc.ge>
2023-06-09 12:07:26 +0400

    Create table

--- src/components/Game/Drawer.js
+++ src/components/Game/Drawer.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import "./Drawer.css";
 
-const Drawer = () => {
-  return <div className="drawer"></div>;
+const Drawer = (props) => {
+  const [drawerInside, setDrawerInside] = useState("");
+
+  const putClickHendler = (e) => {
+    // if (props.playerTurn === "player1") {
+    //   setDrawerInside(<img className="x" src={X} alt="X logo" />);
+    //   props.onChangeTurn();
+    // } else {
+    //   setDrawerInside(<img className="x" src={O} alt="O logo" />);
+    //   props.onChangeTurn();
+    // }
+  };
+
+  return (
+    <div onClick={putClickHendler} className="drawer">
+      {props.children}
+    </div>
+  );
 };
 
 export default Drawer;
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,25 +1,36 @@
 import "./Game.css";
 import Table from "./Table";
 import Drawer from "./Drawer";
+import { useState } from "react";
+import X from "../../img/X.svg";
+import O from "../../img/O.svg";
 
 const Game = () => {
+  const [table, setTable] = useState([
+    { drawer: 1, value: X },
+    { drawer: 2, value: O },
+    { drawer: 3, value: X },
+    { drawer: 4, value: X },
+    { drawer: 5, value: O },
+    { drawer: 6, value: X },
+    { drawer: 7, value: X },
+    { drawer: 8, value: O },
+    { drawer: 9, value: O },
+  ]);
+
+  const [turn, setTurn] = useState("player1");
+
+  const ChangeTurnHendler = () => {
+    // setTurn((prev) => !prev);
+  };
+
   return (
     <>
       <Table>
         <div className="rows">
-          <Drawer />
-          <Drawer />
-          <Drawer />
-        </div>
-        <div className="rows">
-          <Drawer />
-          <Drawer />
-          <Drawer />
-        </div>
-        <div className="rows">
-          <Drawer />
-          <Drawer />
-          <Drawer />
+          {table.map((drawer) => (
+            <Drawer key={Math.random()} />
+          ))}
         </div>
       </Table>
     </>
