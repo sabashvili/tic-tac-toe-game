0f7b1158ac03fc5b39bdfa28368259a117662f54
sabashvili <sabashvili.shota@gmail.com>
2023-06-11 01:36:56 +0400

    Create winner discover logic

--- src/components/Game/Drawer.js
+++ src/components/Game/Drawer.js
@@ -1,17 +1,12 @@
-import { useState } from "react";
 import "./Drawer.css";
 
 const Drawer = (props) => {
-  const [drawerInside, setDrawerInside] = useState("");
-
   const putClickHendler = (e) => {
-    // if (props.playerTurn === "player1") {
-    //   setDrawerInside(<img className="x" src={X} alt="X logo" />);
-    //   props.onChangeTurn();
-    // } else {
-    //   setDrawerInside(<img className="x" src={O} alt="O logo" />);
-    //   props.onChangeTurn();
-    // }
+    if (!props.currentDrawer.value) {
+      props.onSetValue(props.currentDrawer.drawerNumber);
+      props.onTurnChange();
+      props.onWinnerDisc(props.currentDrawer.drawerNumber);
+    }
   };
 
   return (
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -7,21 +7,63 @@ import O from "../../img/O.svg";
 
 const Game = () => {
   const [table, setTable] = useState([
-    { drawer: 1, value: X },
-    { drawer: 2, value: O },
-    { drawer: 3, value: X },
-    { drawer: 4, value: X },
-    { drawer: 5, value: O },
-    { drawer: 6, value: X },
-    { drawer: 7, value: X },
-    { drawer: 8, value: O },
-    { drawer: 9, value: O },
+    { drawerNumber: 1, value: "" },
+    { drawerNumber: 2, value: "" },
+    { drawerNumber: 3, value: "" },
+    { drawerNumber: 4, value: "" },
+    { drawerNumber: 5, value: "" },
+    { drawerNumber: 6, value: "" },
+    { drawerNumber: 7, value: "" },
+    { drawerNumber: 8, value: "" },
+    { drawerNumber: 9, value: "" },
   ]);
 
-  const [turn, setTurn] = useState("player1");
+  const [winVariations] = useState([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ]);
+
+  const [winner, setWinner] = useState("");
+  const [turn, setTurn] = useState("player-1");
+
+  const winnerDiscover = (currDrawerNumb) => {
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (winVariations[i][j] === currDrawerNumb) {
+          winVariations[i][j] = turn === "player-1" ? "X" : "O";
+        }
+      }
+      if (winVariations[i].every((el) => el === "X")) {
+        setWinner("Player-1");
+      } else if (winVariations[i].every((el) => el === "O")) {
+        setWinner("Player-2");
+      }
+    }
+  };
 
   const ChangeTurnHendler = () => {
-    // setTurn((prev) => !prev);
+    if (turn === "player-1") {
+      setTurn("player-2");
+    } else {
+      setTurn("player-1");
+    }
+  };
+
+  const setDrawerValue = (drawerNumber) => {
+    const currTable = table.map((el) => {
+      if (el.drawerNumber !== drawerNumber) {
+        return el;
+      } else {
+        return { drawerNumber: el.drawerNumber, value: turn === "player-1" ? X : O };
+      }
+    });
+    setTable(currTable);
   };
 
   return (
@@ -29,7 +71,15 @@ const Game = () => {
       <Table>
         <div className="rows">
           {table.map((drawer) => (
-            <Drawer key={Math.random()} />
+            <Drawer
+              key={Math.random()}
+              currentDrawer={drawer}
+              onSetValue={setDrawerValue}
+              onTurnChange={ChangeTurnHendler}
+              onWinnerDisc={winnerDiscover}
+            >
+              <img className="x" src={drawer.value} />
+            </Drawer>
           ))}
         </div>
       </Table>
